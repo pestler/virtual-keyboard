be00f7cb161bdee27c7d1925d9421746e3834fea
Aleh Kuis <pestleralex@yandex.by>
2023-04-28 18:04:04 +0300

    init: add files and eslint
    init: add files and eslint

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import language from './en-ru.js';
+import Keyboard from './keyboard.js';
+// import { set, get } from "./storage.js";
+
+const rowsButtons = [
+  ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+  ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+  ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+  ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+  ['ControlLeft', 'WinLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
+];
+const lang = language.en;
+
+new Keyboard(rowsButtons).init(lang).generatePage();
