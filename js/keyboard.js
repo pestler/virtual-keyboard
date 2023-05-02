f057bd46437bd53a59a01760f917cb394b81dbc6
olezhenka <oleh@tut.by>
2023-05-02 16:02:37 +0300

    feat: add button color and enter text

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,7 +9,7 @@ const MSGS = [MSG_EN, MSG_RU];
 const main = createEl('main', 'main', [createEl('h1', 'title', 'virtual-keyboard')]);
 
 const footer = createEl('footer', 'footer', [createEl('h3', 'title', 'Virtual keyboard for Windows'),
-  createEl('p', 'title-lang', MSGS[0])]);
+createEl('p', 'title-lang', MSGS[0])]);
 
 export default class Keyboard {
   constructor(rowsButtons) {
@@ -50,5 +50,40 @@ export default class Keyboard {
         }
       });
     });
+    document.addEventListener('keydown', this.handleEvent);
+    document.addEventListener('keyup', this.handleEvent);
+    this.container.onmousedown = this.mouseHandleEvent;
+    this.container.onmouseup = this.mouseHandleEvent;
   }
+
+  handleEvent = (event) => {
+    event.stopPropagation();
+    const { code, type } = event;
+    const objKey = this.keyButtons.find((key) => key.code === code);
+    this.output.focus();
+    objKey.div.classList.toggle('active');
+
+    if (code.match(/Alt/)) this.altKey = true;
+    if (code.match(/Shift/)) this.shiftKey = true;
+    //if (code.match(/Alt/) && this.shiftKey) this.changeLanguage();
+    if (code.match(/Shift/) && this.altKey) this.changeLanguage();
+    //  console.log(code, type, objKey, 555);
+  };
+
+  /*  mouseHandleEvent(event) {
+   } */
+
+  changeLanguage = () => {
+    this.keyButtons.forEach((but) => {
+      const objKey = this.keyBase.find((key) => key.code === but.code);
+      if (!objKey) return;
+      console.log(objKey.large);
+      but.large = objKey.large;
+      but.small = objKey.small;
+
+      if (objKey.large && objKey.large.match((/[a-zA-Zа-яёЁ]/g))) {
+        //but.topSymbol.innerHTML = objKey.large;
+      }
+    });
+  };
 }
